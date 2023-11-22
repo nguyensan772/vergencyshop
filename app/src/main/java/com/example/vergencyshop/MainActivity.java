79cb97a11b68db91795adb7f88acf71244f0fcc4
tampdph33277 <tampdph33277@fpt.edu.vn>
2023-11-22 14:33:24 +0700

    update branch tamfpt

--- app/src/main/java/com/example/vergencyshop/MainActivity.java
+++ app/src/main/java/com/example/vergencyshop/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+// tamfpt
 
 
         drawerLayout = findViewById(R.id.layout_all);
